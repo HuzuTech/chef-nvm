87f6826f2c3ca86b4b7cec4e913b9dc6a64331dc
Robert Dallas Gray <robert.dallas.gray@huzutech.com>
2013-05-03 12:57:03 +0100

    Working install recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,54 +13,90 @@ package 'git-core'
 $user  = node['nvm']['user']
 $group = node['nvm']['group'] || $user
 $home  = node['nvm']['home'] || "/home/#{$user}"
+$env   = { 'HOME' => $home }
+$profile = "#{$home}/.bashrc"
+$flags = "-x"
+$nvm_sh = "#{$home}/.nvm/nvm.sh"
+$nvm_source = "source #{$nvm_sh}"
 
-execute "install-nvm" do
+bash "clone-nvm" do
   user  $user
   group $group
   cwd   $home
+  environment $env
+  flags $flags
 
-  command <<-EOF
-if [ ! -d #{$home}/.nvm ]
-  then
-    git clone git://github.com/creationix/nvm.git #{$home}/.nvm
-fi
-
-grep nvm.sh #{$home}/.bashrc
-
-if [ $? -eq 1 ]
-  then
-    echo '\n\n. ~/.nvm/nvm.sh' >> #{$home}/.bashrc
-    . #{$home}/.nvm/nvm.sh
-fi
-  EOF
-
-  notifies :run, "execute[install-nodes]", :immediately
-  not_if "test -d #{$home}/.nvm"
+  creates "#{$home}/.nvm"
+  code "git clone git://github.com/creationix/nvm.git #{$home}/.nvm"
+  notifies :run, "bash[install-nvm]", :immediately
 end
 
-execute "install-nodes" do
+bash "install-nvm" do
   user  $user
   group $group
   cwd   $home
+  environment $env
+  flags $flags
 
-  @all_node_versions = Array(node['nvm']['node_versions'])
+  code "#{$home}/.nvm/install.sh && #{$nvm_source}"
+  notifies :run, "bash[activate-nvm]", :immediately
+end
 
-  command @all_node_versions.map { |v| "nvm install #{v}" }.join("\n").strip
+bash "activate-nvm" do
+  user  $user
+  group $group
+  cwd   $home
+  environment $env
+  flags $flags
 
-  if node['nvm']['default_node_version'] || @all_node_versions.count > 0
-    notifies :run, "execute[make-default-node-version]", :immediately
+  code <<-EOF
+if ! grep -qc 'nvm.sh' #{$profile}; then
+  echo #{$nvm_source} >> #{$profile}
+fi
+  EOF
+  notifies :run, "bash[install-nodes]", :immediately
+end
+
+bash "install-nodes" do
+  user  $user
+  group $group
+  cwd   $home
+  environment $env
+  flags $flags
+
+  versions = Array(node['nvm']['node_versions'])
+
+  code versions.map { |v| "#{$nvm_source} && nvm install #{v}" }.join("\n").strip
+  if node['nvm']['default_node_version'] || versions.count > 0
+    notifies :run, "bash[make-default-node-version]", :immediately
   end
-
   action :nothing
 end
 
-execute "make-default-node-version" do
+bash "make-default-node-version" do
   user  $user
   group $group
   cwd   $home
+  environment $env
+  flags $flags
 
-  @default_node_version = node['nvm']['default_node_version'] || Array(node['nvm']['node_versions']).first
-  command "nvm alias default #{@default_node_version}"
+  version = node['nvm']['default_node_version'] || Array(node['nvm']['node_versions']).first
+  code "#{$nvm_source} && nvm alias default #{version}"
+  notifies :run, "bash[install-npm]", :immediately
+  action :nothing
+end
 
+bash "install-npm" do
+  user  $user
+  group $group
+  cwd   $home
+  environment $env
+  flags $flags
+
+  code <<-EOF
+git clone git://github.com/isaacs/npm.git /tmp/npm && \
+#{$nvm_source} && nvm use default && cd /tmp/npm \
+&& make install && rm -Rf /tmp/npm
+  EOF
   action :nothing
 end
